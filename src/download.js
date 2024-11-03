5dfe0c8d6668c29f39e527fbbf1c5e5a4a0e913b
Clartik <yg.kartik@gmail.com>
2024-11-02 23:27:14 -0700

    Updated link

--- src/download.js
+++ src/download.js
@@ -1,5 +1,5 @@
 const downloadWindowsBtn = document.getElementById('download-btn');
 
 downloadWindowsBtn.addEventListener('click', () => {
-    window.location.href = 'https://canvas-hw-reminder.s3.us-west-1.amazonaws.com/Canvas-HW-Reminder-Setup-0.8.0-beta.1.exe';
+    window.location.href = 'https://canvas-hw-reminder.s3.us-west-1.amazonaws.com/Canvas-HW-Reminder-Setup.exe';
 })
