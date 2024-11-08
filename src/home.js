184673b8fffa19bc80f8be16f461fccf3463009e
Clartik <yg.kartik@gmail.com>
2024-11-08 10:55:19 -0800

    Added more toggles and images

--- src/home.js
+++ src/home.js
@@ -12,3 +12,33 @@ alwaysExpandCardsCheckbox.addEventListener('click', async (event) => {
         alwaysExpandCardsAfterImg.classList.remove('active');
     }
 })
+
+showExactDueDatesCheckbox = document.getElementById('show-exact-due-dates-checkbox');
+showExactDueDatesBeforeImg = document.getElementById('show-exact-due-dates-before-img');
+showExactDueDatesAfterImg = document.getElementById('show-exact-due-dates-after-img');
+
+showExactDueDatesCheckbox.addEventListener('click', async (event) => {
+    if (showExactDueDatesCheckbox.checked) {
+        showExactDueDatesBeforeImg.classList.remove('active');
+        showExactDueDatesAfterImg.classList.add('active');
+
+    } else {
+        showExactDueDatesBeforeImg.classList.add('active');
+        showExactDueDatesAfterImg.classList.remove('active');
+    }
+})
+
+dontRemindNoSubmissionsCheckbox = document.getElementById('dont-remind-no-submissions-checkbox');
+dontRemindNoSubmissionsBeforeImg = document.getElementById('dont-remind-no-submissions-before-img');
+dontRemindNoSubmissionsAfterImg = document.getElementById('dont-remind-no-submissions-after-img');
+
+dontRemindNoSubmissionsCheckbox.addEventListener('click', async (event) => {
+    if (dontRemindNoSubmissionsCheckbox.checked) {
+        dontRemindNoSubmissionsBeforeImg.classList.remove('active');
+        dontRemindNoSubmissionsAfterImg.classList.add('active');
+
+    } else {
+        dontRemindNoSubmissionsBeforeImg.classList.add('active');
+        dontRemindNoSubmissionsAfterImg.classList.remove('active');
+    }
+})
